112e97ac792c8cf712a3892164d5d0100013e87f
Behar Osmani <behaar.o@gmail.com>
2022-01-08 21:16:15 +0100

    initial commit

---
+++ guess_number_GUI.py
@@ -0,0 +1,104 @@
+from tkinter import *
+from tkinter import messagebox
+import random
+
+class GuessNumber:
+    random_number=0 
+    number_of_guesses=0 
+    # var=''
+    # info=''
+    def __init__(self,root,title,geometry):
+        
+        self.root=root
+        self.root.title(title)
+        self.root.geometry(geometry)
+
+
+        self.label_enter_number=Label(self.root,text="Pick a number grater than zero")
+        self.label_enter_number.pack(pady=5)
+
+        self.input_top_range_number=Entry(self.root,width=20)
+        self.input_top_range_number.pack(pady=2)
+
+        btnTopRange=Button(self.root,text="Enter top range",command=self.topRangeFunc,width=12)
+        btnTopRange.pack(pady=5)
+
+
+
+        self.label_guess_number=Label(self.root,text="Enter the guessing number")
+        self.label_guess_number.pack(pady=5)
+
+        self.input_guess_number=Entry(self.root,width=20)
+        self.input_guess_number.pack(pady=2)
+
+        self.btnGuess=Button(self.root,text="Guess",command=self.mainFunction,width=12)
+        self.btnGuess.pack(pady=5)
+
+
+        self.label_nr_guesses=Label(self.root,text="Number of guesses")
+        self.label_nr_guesses.pack(pady=8)
+
+        # self.var=StringVar()
+        # self.label_value=Label(self.root,textvariable=self.var,relief=RAISED,width=15)
+        # self.label_value.pack(pady=3)
+
+        # self.info=StringVar()
+        # self.label_info=Label(self.root,textvariable=self.info,text="Guess the number Game",relief=RAISED,font=('Helvetica',14)).pack(pady=20)
+
+        self.label_info=Label(self.root,text="Guess the number Game",font=("Helvetica",14))
+        self.label_info.pack(pady=20)
+        
+        self.root.resizable(True,True)
+        self.root.mainloop()
+
+    # @classmethod
+    # def setVar(cls):
+    #     cls.var.set(cls.number_of_guesses)
+
+    def topRangeFunc(self):
+       
+        try:
+            top_range_number=int(self.input_top_range_number.get()) 
+            self.number_of_guesses=0
+            # self.var.set(self.number_of_guesses)
+            self.label_nr_guesses.config(text="Number of guesses:"+str(self.number_of_guesses))
+
+            if(top_range_number<=0):
+                messagebox.showerror("Error",'Number should be higher than zero')
+            else:
+                self.random_number=random.randint(0,top_range_number)
+        except:
+             messagebox.showerror("Error","The value must be a number")
+
+    
+    def mainFunction(self):
+        
+
+        try:
+            guess_number=int(self.input_guess_number.get())
+            self.number_of_guesses+=1
+            self.label_nr_guesses.config(text="Number of guesses:"+str(self.number_of_guesses))
+            # self.label_guess_number.config(text="Number of guesses:"+self.number_of_guesses)
+            # self.var.set(self.number_of_guesses)
+
+            if(self.random_number==guess_number): 
+                # self.info.set("Your got it!")
+                self.label_info.config(text="You got it!")
+        
+            elif(guess_number<self.random_number): 
+                # self.info.set("Your guess is below the number")
+                 self.label_info.config(text="Your guess is below the number")
+
+            else:
+                # self.info.set("Your guess is above the number")
+                self.label_info.config(text="Your guess is above the number")
+        except:
+            messagebox.showerror("Error","The value must be a number")
+        
+
+# class Second(GuessNumber):
+#     def __init__(self, root, title, geometry):
+#         super().__init__(root, title, geometry)
+            
+root=Tk()
+g=GuessNumber(root,'Guess Number','350x350')
